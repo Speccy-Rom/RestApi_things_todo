1fedbc02b50fc538a6c9e61b9657b719a9732b44
Speccy-Rom <speccy.rom@gmail.com>
2021-07-28 23:52:57 +0300

    added project structure & handlers initialization

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"github.com/speccy-rom/RestApi_things_todo"
+	"github.com/speccy-rom/RestApi_things_todo/pkg/handler"
+	"log"
+)
 
+func main() {
+	handlers := new(handler.Handler)
+
+	srv := new(todo.Server)
+	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }

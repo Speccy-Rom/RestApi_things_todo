fe4f8a39e7f4b9840b907580a9aacbcd351b41a7
Speccy-Rom <speccy.rom@gmail.com>
2021-07-29 21:28:19 +0300

    added configuration files and viper initialization

--- cmd/main.go
+++ cmd/main.go
@@ -5,16 +5,27 @@ import (
 	"github.com/speccy-rom/RestApi_things_todo/pkg/handler"
 	"github.com/speccy-rom/RestApi_things_todo/pkg/repository"
 	"github.com/speccy-rom/RestApi_things_todo/pkg/service"
+	"github.com/spf13/viper"
 	"log"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
+
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }

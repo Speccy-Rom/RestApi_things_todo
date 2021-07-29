ebe008895c4f57852e36f3908f021f796c4a45c8
Speccy-Rom <speccy.rom@gmail.com>
2021-07-29 19:13:56 +0300

    added repository and service interfaces

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,15 @@ package main
 import (
 	"github.com/speccy-rom/RestApi_things_todo"
 	"github.com/speccy-rom/RestApi_things_todo/pkg/handler"
+	"github.com/speccy-rom/RestApi_things_todo/pkg/repository"
+	"github.com/speccy-rom/RestApi_things_todo/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
 
 	srv := new(todo.Server)
 	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {

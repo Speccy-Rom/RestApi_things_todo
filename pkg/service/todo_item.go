69e68ab3f636218ae5fae9380abbb1417af1cb75
Speccy-Rom <speccy.rom@gmail.com>
2021-08-08 17:21:51 +0300

    implemented create & get all items logic

---
+++ pkg/service/todo_item.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/speccy-rom/RestApi_things_todo"
+	"github.com/speccy-rom/RestApi_things_todo/pkg/repository"
+)
+
+type TodoItemService struct {
+	repo     repository.TodoItem
+	listRepo repository.TodoList
+}
+
+func NewTodoItemService(repo repository.TodoItem, listRepo repository.TodoList) *TodoItemService {
+	return &TodoItemService{repo: repo, listRepo: listRepo}
+}
+
+func (s *TodoItemService) Create(userId, listId int, item todo.TodoItem) (int, error) {
+	_, err := s.listRepo.GetById(userId, listId)
+	if err != nil {
+		// list does not exists or does not belongs to user
+		return 0, err
+	}
+
+	return s.repo.Create(listId, item)
+}
+
+func (s *TodoItemService) GetAll(userId, listId int) ([]todo.TodoItem, error) {
+	return s.repo.GetAll(userId, listId)
+}
